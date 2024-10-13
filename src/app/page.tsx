a3d4281b3a75bf553e299a757effa3c3432cfd45
Jonas List <Jonas.List.1289@gmail.com>
2024-10-13 09:28:07 +0200

    Add vertical tabs example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 const LandingPage = async () => {
   return (
     <main className="container flex flex-col gap-6">
+      {/* Horizontal tabs */}
       <Tabs defaultValue="option1">
         <TabsList>
           <TabsTrigger value="option1">Option 1</TabsTrigger>
@@ -27,6 +28,33 @@ const LandingPage = async () => {
         </TabsContent>
         <TabsContent value="option4">
           <TabContent name="Option 1" />
+        </TabsContent>
+      </Tabs>
+
+      {/* Vertical tabs */}
+      <Tabs
+        orientation="vertical"
+        defaultValue="option1"
+        className="flex flex-row gap-2"
+      >
+        <TabsList className="m-0 flex flex-col items-stretch w-full h-auto">
+          <TabsTrigger value="option1">Option 1</TabsTrigger>
+          <TabsTrigger value="option2">Option 2</TabsTrigger>
+          <TabsTrigger value="option3">Option 3</TabsTrigger>
+          <TabsTrigger value="option4">Option 4</TabsTrigger>
+        </TabsList>
+
+        <TabsContent className="mt-0" value="option1">
+          <TabContent name="Option 1" />
+        </TabsContent>
+        <TabsContent className="mt-0" value="option2">
+          <TabContent name="Option 2" />
+        </TabsContent>
+        <TabsContent className="mt-0" value="option3">
+          <TabContent name="Option 3" />
+        </TabsContent>
+        <TabsContent className="mt-0" value="option4">
+          <TabContent name="Option 4" />
         </TabsContent>
       </Tabs>
 
